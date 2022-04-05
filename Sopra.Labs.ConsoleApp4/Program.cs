d0433bd86d852aa8c5259d5ff140eb86d0eccafc
Yanran Chen Weng <yanran.cheng@soprasteria.com>
2022-04-05 11:37:50 +0200

    Creación de UpdateStudent y DeleteStudent

--- Sopra.Labs.ConsoleApp4/Program.cs
+++ Sopra.Labs.ConsoleApp4/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Text;
 using Newtonsoft.Json;
 
@@ -14,7 +15,9 @@ namespace Sopra.Labs.ConsoleApp4
         {
             //ZipInfo();
             //GetStudent();
-            PostStudent();
+            //PostStudent();
+            //UpdateStudent();
+            //DeleteStudent();
         }
 
         static void ZipInfo()
@@ -40,14 +43,14 @@ namespace Sopra.Labs.ConsoleApp4
                 Console.WriteLine($"Código postal: {data.postCode}");
                 Console.WriteLine($"País: {data.country} ({data.countryAbbreviation})");
 
-                foreach(var i in data.places)
+                foreach (var i in data.places)
                 {
                     Console.WriteLine($"Lugar: {i.placeName}");
                     Console.WriteLine($"Ciudad: {i.state} ({i.stateAbbreviation})");
                     Console.WriteLine($"Posición: {i.longitude}, {i.latitude}");
                     Console.WriteLine();
                 }
-                
+
                 Console.WriteLine(Environment.NewLine);
             }
             else Console.WriteLine($"Error: {response.StatusCode}");
@@ -58,7 +61,7 @@ namespace Sopra.Labs.ConsoleApp4
             var http = new HttpClient();
             http.BaseAddress = new Uri("http://school.labs.com.es/api/students/");
 
-            Console.Write("Estudiante: ");
+            Console.Write("ID del estudiante a consultar: ");
             HttpResponseMessage response = http.GetAsync(Console.ReadLine()).Result;
 
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
@@ -84,14 +87,11 @@ namespace Sopra.Labs.ConsoleApp4
             var http = new HttpClient();
             http.BaseAddress = new Uri("http://school.labs.com.es/api/students/");
 
-            http.DefaultRequestHeaders.Add("Accept", "application/json");
-            http.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
             var student = new Student()
             {
                 Id = 0,
                 Firstname = "Miguel",
-                Lastname = "Cervantes",
+                Lastname = "a borrar",
                 DateOfBirth = new DateTime(1547, 10, 9),
                 ClassId = 2
             };
@@ -102,9 +102,64 @@ namespace Sopra.Labs.ConsoleApp4
 
             if (response.StatusCode == HttpStatusCode.Created)
             {
-                var responseBody = response.Content.ReadAsStringAsync().Result;
-                Console.WriteLine(responseBody);
+                var data = JsonConvert.DeserializeObject<Student>(response.Content.ReadAsStringAsync().Result);
+
+                Console.WriteLine($"ID: {data.Id}");
+                Console.WriteLine($"Nombre completo: {data.Firstname} {data.Lastname}");
+                Console.WriteLine($"Fecha de nacimiento: {data.DateOfBirth}");
+                Console.WriteLine($"clase: {data.ClassId} {data.Class}");
             }
+            else Console.WriteLine($"Error: {response.StatusCode}");
+        }
+
+        static void UpdateStudent()
+        {
+            var http = new HttpClient();
+            http.BaseAddress = new Uri("http://school.labs.com.es/api/students/");
+
+            Console.Write("Id del estudiante a modificar: ");
+            var data = http.GetFromJsonAsync<Student>(Console.ReadLine()).Result;
+
+            string temp = "";
+
+            Console.Write("Nombre nuevo: ");
+            string firstName = (temp = Console.ReadLine()) == "" ? data.Firstname : temp;
+            Console.Write("Apellidos nuevos: ");
+            string lastName = (temp = Console.ReadLine()) == "" ? data.Lastname : temp;
+            Console.Write("Fecha de nacimiento nuevo: ");
+            string dateOfBirth = (temp = Console.ReadLine()) == "" ? data.DateOfBirth.ToString() : temp;
+            Console.Write("Id de clase nuevo: ");
+            string classID = (temp = Console.ReadLine()) == "" ? data.ClassId.ToString() : temp;
+
+            var student = new Student()
+            {
+                Id = data.Id,
+                Firstname = firstName,
+                Lastname = lastName,
+                DateOfBirth = DateTime.Parse(dateOfBirth),
+                ClassId = Int32.Parse(classID),
+                Class = null
+            };
+
+            var content = new StringContent(JsonConvert.SerializeObject(student), Encoding.UTF8, "application/json");
+            var response = http.PutAsync(data.Id.ToString(), content).Result;
+
+            Console.ReadKey();
+
+            if (response.IsSuccessStatusCode) Console.WriteLine($"Estudiante {data.Id} modificado.");
+            else Console.WriteLine($"Error: {response.StatusCode}");
+        }
+
+        static void DeleteStudent()
+        {
+            var http = new HttpClient();
+            http.BaseAddress = new Uri("http://school.labs.com.es/api/students/");
+
+            Console.Write("Id del estudiante a borrar: ");
+            string id = Console.ReadLine();
+
+            var response = http.DeleteAsync(id).Result;
+            if (response.IsSuccessStatusCode) Console.WriteLine($"Estudiante {id} eliminado.");
             else Console.WriteLine($"Error: {response.StatusCode}");
         }
     }
