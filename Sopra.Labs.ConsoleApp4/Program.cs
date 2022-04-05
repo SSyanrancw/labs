bb57b684a971de5810921832f2bb42080006844c
Yanran Chen Weng <yanran.cheng@soprasteria.com>
2022-04-05 17:01:12 +0200

    Más labs

--- Sopra.Labs.ConsoleApp4/Program.cs
+++ Sopra.Labs.ConsoleApp4/Program.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text;
 using Newtonsoft.Json;
+using System.Linq;
+using System.Collections;
 
 namespace Sopra.Labs.ConsoleApp4
 {
@@ -18,6 +19,9 @@ namespace Sopra.Labs.ConsoleApp4
             //PostStudent();
             //UpdateStudent();
             //DeleteStudent();
+            LoginEMT();
+            //ArrivalBus();
+            Parking();
         }
 
         static void ZipInfo()
@@ -144,8 +148,6 @@ namespace Sopra.Labs.ConsoleApp4
             var content = new StringContent(JsonConvert.SerializeObject(student), Encoding.UTF8, "application/json");
             var response = http.PutAsync(data.Id.ToString(), content).Result;
 
-            Console.ReadKey();
-
             if (response.IsSuccessStatusCode) Console.WriteLine($"Estudiante {data.Id} modificado.");
             else Console.WriteLine($"Error: {response.StatusCode}");
         }
@@ -161,6 +163,105 @@ namespace Sopra.Labs.ConsoleApp4
             var response = http.DeleteAsync(id).Result;
             if (response.IsSuccessStatusCode) Console.WriteLine($"Estudiante {id} eliminado.");
             else Console.WriteLine($"Error: {response.StatusCode}");
+        }
+
+        static string TokenEMT = "";
+
+        static void LoginEMT()
+        {
+            var http = new HttpClient();
+            http.BaseAddress = new Uri("https://openapi.emtmadrid.es/v2/");
+
+            try
+            {
+                http.DefaultRequestHeaders.Add("Accept", "application/json");
+                Console.Write("X-ClientID: ");
+                http.DefaultRequestHeaders.Add("X-ClientId", Console.ReadLine());
+                Console.Write("passKey: ");
+                http.DefaultRequestHeaders.Add("passKey", Console.ReadLine());
+
+                HttpResponseMessage response = http.GetAsync("mobilitylabs/user/login/").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = response.Content.ReadAsStringAsync().Result;
+
+                    var data = JsonConvert.DeserializeObject<dynamic>(content);
+
+                    foreach (var i in data["data"]) Console.WriteLine(TokenEMT = i["accessToken"]);
+                }
+                else Console.WriteLine($"Error: {response.StatusCode}");
+
+                Console.WriteLine(Environment.NewLine);
+            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+        }
+
+        static void ArrivalBus()
+        {
+            var http = new HttpClient();
+            http.BaseAddress = new Uri("https://openapi.emtmadrid.es/v2/");
+
+            try
+            {
+                http.DefaultRequestHeaders.Add("accessToken", TokenEMT);
+
+                var body = new
+                {
+                    cultureInfo = "ES",
+                    Text_StopRequired_YN = "Y",
+                    Text_EstimationsRequired_YN = "Y",
+                    Text_IncidencesRequired_YN = "Y",
+                    DateTime_Referenced_Incidencies_YYYYMMDD = DateTime.Now.ToString("yyyyMMdd")
+                };
+
+                var content = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
+                Console.Write("Número de parada: ");
+                var response = http.PostAsync($"transport/busemtmad/stops/{Console.ReadLine()}/arrives/", content).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = JsonConvert.DeserializeObject<dynamic>(response.Content.ReadAsStringAsync().Result);
+
+                    if(data.code == "00")
+                    {
+                        foreach (var i in data["data"])
+                        {
+                            foreach (var j in i["Arrive"]) Console.WriteLine($"La línea: {j["line"]} llegará en {(j["estimateArrive"] / 60).ToString("N0")} minutos");
+                        }
+                    }
+                    else Console.WriteLine("No existe la parada solicitada.");
+                }
+                else Console.WriteLine($"Error: {response.StatusCode}");
+            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+        }
+
+        static void Parking()
+        {
+            var http = new HttpClient();
+            http.BaseAddress = new Uri("https://openapi.emtmadrid.es/v2/");
+
+            try
+            {
+                http.DefaultRequestHeaders.Add("accessToken", TokenEMT);
+
+                var response = http.GetAsync("citymad/places/parkings/availability/").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = JsonConvert.DeserializeObject<dynamic>(response.Content.ReadAsStringAsync().Result);
+
+                    var freeParking = ((IEnumerable)data["data"]).Cast<dynamic>()
+                        .Where(r => r["freeParking"] != null)
+                        .Sum(r => r["freeParking"]);
+
+                    Console.WriteLine($"Número de plazas de parkings libres: {freeParking} - {(Convert.ToDateTime(data["datetime"])).ToString("dd-MM-yyyy HH:mm")}");
+                    
+                }
+                else Console.WriteLine($"Error: {response.StatusCode}");
+            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
         }
     }
 
